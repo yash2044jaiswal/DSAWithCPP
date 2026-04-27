8a922bee9e3f6fd91138aaae346aa9ebd83e05ae
yash2044jaiswal <yash2044jaiswal@gmail.com>
2026-04-27 16:39:06 +0000

    String

---
+++ Arrays/Array_Search.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Array{
+    public:
+    int  search(vector<int>& arr,int x){
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]==x){
+                
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+int main(){
+    Array a;
+    vector<int>ans;
+    ans.push_back(2);
+    ans.push_back(4);
+    ans.push_back(45);
+    ans.push_back(5);
+    cout<<"searching:"<<a.search(ans,5);
+
+    return 0;
+
+}

4aa09929e2d534e107ef7260cf181fa2fa9cb18b
yash2044jaiswal <yash2044jaiswal@gmail.com>
2026-06-19 22:26:59 +0530

    Array

---
+++ Arrays/BasicsLevel/Alternatives.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    vector<int> getAlternates(vector<int> &arr) {
+        vector<int> res;
+        int i=0;
+        while(i<arr.size()){
+            res.push_back(arr[i]);
+            i=i+2;
+        }
+        return res;
+    }
+};
+int main(){
+    Solution s1;
+    vector<int>array={1,12,11,4,45,6};
+    vector <int>result=s1.getAlternates(array);
+    for(int i=0;i<result.size();i++){
+        cout<<result[i]<<" ";
+    }
+    return 0;
+}

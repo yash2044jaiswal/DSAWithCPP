08b9ae0ae31aa5a7ed3e065c56116b2fc69efb0c
yash2044jaiswal <yash2044jaiswal@gmail.com>
2026-06-23 10:40:46 +0000

    ArrayBasics

---
+++ Arrays/BasicsLevel/ArrayinsertIndex.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    void insertAtIndex(vector<int> &arr, int index, int val) {
+        arr.resize(arr.size()+1);
+        for(int i=arr.size()-1;i>=index;i--){
+            arr[i]=arr[i-1];
+        }
+        
+        arr[index]=val;
+        
+    }
+};
+int main(){
+    Solution s1;
+    vector<int>array={12,34,11,32,33};
+    s1.insertAtIndex(array,3,50);
+    for(int i=0;i<array.size();i++){
+        cout<<array[i]<<" ";
+    }
+    return 0;
+}
---
+++ Arrays/BasicsLevel/CountSmallerArray.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    int countOfElements(int x, vector<int> &arr) {
+        // code here
+        int count=0;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]<= x){
+                count++;
+            }
+        }
+        return count;
+    }
+};
+int main(){
+    Solution s1;
+    vector<int>arr={12,3,2,123,3,9};
+    cout<<"count of smaller:"<<s1.countOfElements(5,arr)<<endl;
+    return 0;
+
+}

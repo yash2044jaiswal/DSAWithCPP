020bc370fd858b39f623960d6f16c398b9bf24ea
yash2044jaiswal <yash2044jaiswal@gmail.com>
2026-06-24 22:37:09 +0530

    Add function to convert 0s to 5s in an integer
    Implemented a function to replace all occurrences of 0 with 5 in an integer.

---
+++ Arrays/BasicsLevel/Replace0with5.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    int convertFive(int n) {
+        // code here
+        string str=to_string(n);
+        for(int i=0;i<str.size();i++){
+            if(str[i]=='0'){
+                str[i]='5';
+            }
+        }
+      
+        return stoi(str);
+    }
+};
+int main(){
+    Solution s1;
+    cout<<"replaceble:"<<s1.convertFive(10004)<<endl;
+
+    return 0;
+}

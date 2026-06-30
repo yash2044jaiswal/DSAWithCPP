86942767065826131cbf98d20fccf5e114bec170
yash2044jaiswal <yash2044jaiswal@gmail.com>
2026-06-30 05:00:00 +0000

    Day!5DSA

---
+++ Arrays/BasicsLevel/LastCoinGameAlternates.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    int coin(vector<int>& arr) {
+        // code here
+        int min=arr[0];
+        if(arr.size()==1){
+            return arr[0];
+        }else{
+            for(int i=0;i<arr.size();i++){
+                if(arr[i]<min){
+                    min=arr[i];
+                }
+            }
+            return min;
+        }
+    }
+};
+int main() {
+    Solution s1;
+    vector<int> array = {5, 3, 8, 1, 4};
+    int lastCoin = s1.coin(array);
+    cout << "The last coin is: " << lastCoin << endl;
+    return 0;
+}
---
+++ Arrays/BasicsLevel/LongestWord.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    string longest(vector<string>& arr) {
+        // code here
+        int length=0;
+        string str1;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i].length()>str1.length()){
+                str1=arr[i];
+            }
+        }
+        return str1;
+    }
+};
+int main() {
+    Solution s1;
+    vector<string> array = {"apple", "banana", "cherry", "date"};
+    string longestWord = s1.longest(array);
+    cout << "The longest word is: " << longestWord << endl;
+    return 0;
+}

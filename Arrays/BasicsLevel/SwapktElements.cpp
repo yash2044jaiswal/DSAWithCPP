2cc654d015ccae8fe8f58eb16db57b7e8cb67738
yash2044jaiswal <yash2044jaiswal@gmail.com>
2026-07-02 19:10:04 +0530

    Add swapKth function to swap k-th elements
    Implemented a function to swap the k-th element with its corresponding element from the end of the array.

---
+++ Arrays/BasicsLevel/SwapktElements.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    void swapKth(vector<int> &arr, int k) {
+        // code here
+        int n=arr.size()-k;
+        int temp=arr[k-1];
+        arr[k-1]=arr[n];
+        arr[n]=temp;
+        
+    }
+};
+int main(){
+    Solution s1;
+    vector<int>arr={12,13,43,43,23,1,3};
+    cout<<"before swaping:"<<endl;
+    for(int i=0;i<arr.size();i++){
+        cout<<arr[i]<<" ";
+    }
+    s1.swapkth(arr,2);
+    cout<<"After swaping:"<<endl;
+    for(int i=0;i<arr.size();i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}

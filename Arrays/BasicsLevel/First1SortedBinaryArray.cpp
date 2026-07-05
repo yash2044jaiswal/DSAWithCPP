4e55875bbd49a375b3d5d511db290242b652cfb7
yash2044jaiswal <yash2044jaiswal@gmail.com>
2026-07-05 14:10:47 +0000

    newproblem2

---
+++ Arrays/BasicsLevel/First1SortedBinaryArray.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    int firstIndex(vector<int> &arr) {
+        // Your code goes here
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]==1){
+                return i;
+                break;
+                
+            }
+        }
+        return -1;
+    }
+};
+int main(){
+    Solution s1;
+    vector<int>res={0, 0, 0, 0, 0, 0, 1, 1, 1, 1};
+    cout<<s1.firstIndex(res);
+    return 0;
+}

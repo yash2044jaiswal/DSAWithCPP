9fe7b7fd4459e4418b070eb7bef3a52e77564399
yash2044jaiswal <yash2044jaiswal@gmail.com>
2026-07-16 15:51:02 +0530

    Refactor factorial function and update main

--- Recursion/PlaylistStriver/Factorial.cpp
+++ Recursion/PlaylistStriver/Factorial.cpp
@@ -1,21 +1,11 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-long long factorial(int n) {
-	if (n <= 1) return 1;
-	return n * factorial(n - 1);
+int factorials(int n){
+    if(n==0)return 1;
+    return n*factorials(n-1);
 }
-
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
-
-	int n;
-	if (!(cin >> n)) return 0;
-	if (n < 0) {
-		cout << "Undefined for negative integers\n";
-		return 0;
-	}
-	cout << factorial(n) << '\n';
-	return 0;
+int main(){
+    cout<<factorials(5);
+    return 0;
 }
